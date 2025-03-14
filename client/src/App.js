ac64a137a583fe2106bfdf7c09340cf312c1a942
heojongwoo010 <jongu010@gmail.com>
2025-03-14 15:52:20 +0900

    Update all files

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import ImportComponent from './R003_ImportComponent';
+import LifecycleEx from './R004_LifecycleEx';
+/* component란, 특정 코드 뭉치를 다른 부분에 이식하거나 재사용하기 위해 사용하는 코드 블록 단위 */
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p> CSS 적용하기 </p>
+      <ImportComponent></ImportComponent>
+      <LifecycleEx></LifecycleEx>
     </div>
   );
 }
